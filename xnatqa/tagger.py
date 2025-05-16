2bbd03889ce21b355c9c7a5b3f540a4ba9262dd2
Kyle Kurkela <kkurkela@bu.edu>
2025-05-16 12:42:20 -0400

    tagger command line tool overhaul

--- xnatqa/tagger.py
+++ xnatqa/tagger.py
@@ -1,6 +1,9 @@
 import os
 import argparse
-from xnatqa.tag import tag_scans
+from xnatqa.tag import generate_tagger_config
+from xnatqa.tag import generate_tagger_yaml
+from xnatqa.tag import update_xnat_tags
+import shutil
 
 def main():
 
@@ -18,7 +21,21 @@ def main():
     dryrun      = args.dryrun
 
     # tag all scans in this session
-    tag_scans(dicom_dir, experiment, working_dir, dryrun)
+     # create a working directory to write temporary files
+    this_session_working_dir = os.path.join(working_dir, 'xnattager', experiment)
+    if os.path.isdir(this_session_working_dir):
+        shutil.rmtree(this_session_working_dir)
+    os.makedirs(this_session_working_dir)
+
+    # generate the xnattag config file
+    generate_tagger_config(dicom_dir, this_session_working_dir)
+    
+    # general the yaml
+    generate_tagger_yaml(this_session_working_dir)
+
+    # update the xnat tags
+    if not dryrun:
+        update_xnat_tags(experiment, this_session_working_dir)
 
 if __name__ == "__main__":
     main()
